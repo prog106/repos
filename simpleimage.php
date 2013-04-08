f8cac1cba286a4cddee980a96c99f6e4bd7ba3d1
prog106 <prog106@gmail.com>
2013-04-08 18:21:43 +0900

    image control modify

--- simpleimage.php
+++ simpleimage.php
@@ -1,14 +1,16 @@
 ﻿<!DOCTYPE html>
 <html lang="utf8">
 <?
+require_once "debug.php";
 class SimpleImage {
     var $image;
     var $image_type;
     
-    function load($filename) { // 이미지정보 처리하기
-        $image_info = getimagesize($filename); // 이미지 정보
-        $this->image_type = $image_info[2]; // 이미지 type
-        if($this->image_type == 'IMAGETYPE_JPEG') { // jpeg
+    function load($filename) { // image_info
+        $image_info = getimagesize($filename);
+        $this->image_type = $image_info[2]; // type
+        //if($this->image_type == 'IMAGETYPE_JPEG') { // jpeg
+        if($this->image_type == '2') { // jpeg
             $this->image = imagecreatefromjpeg($filename);
             $this->ext = ".jpg";
         } else if($this->image_type == 'IMAGETYPE_GIF') { // gif
@@ -31,36 +33,37 @@ class SimpleImage {
         return imagesy($this->image);
     }
     
-    function resizeToWidth($width) { // width size에 맞춰 image size (x,y)바꾸기
+    function resizeToWidth($width) { // width size image size (x,y)
         $ratio = $width / $this->getWidth();
         $height = $this->getHeight() * $ratio;
         $this->resize($width, $height);
     }
     
-    function resizeToHeight($height) { // height size에 맞춰 image size (x,y)바꾸기
+    function resizeToHeight($height) { // height size image size (x,y)
         $ratio = $height / $this->getHeight();
         $width = $this->getWidth() * $ratio;
         $this->resize($width, $height);
     }
     
-    function resize($width, $height) { // image size (x,y) 바꾸기
+    function resize($width, $height) { // image size (x,y)
         $new_image = imagecreatetruecolor($width, $height);
         imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
         $this->image = $new_image;
     }
 
-    function scale($scale) { // %로 image size (x,y) 바꾸기
+    function scale($scale) { // %로 image size (x,y)
         $ratio = $scale / 100;
         $this->resize($this->getWidth() * $ratio, $this->getHeight * $ratio);
     }
     
-    function output($image_type = IMAGETYPE_JPEG) { // image 만들기
+    function output($image_type = IMAGETYPE_JPEG) { // image
         if($image_type == 'IMAGETYPE_JPEG') { // jpeg
             imagejpeg($this->image);
         } else if($this->image_type == 'IMAGETYPE_GIF') { // gif
             imagegif($this->image);
         } else if($this->image_type == 'IMAGETYPE_PNG') { // png
             imagepng($this->image);
+        }
     }
     
     function save($filename, $image_type = 'IMAGETYPE_JPEG', $compression=75, $permissions=null) {
@@ -70,6 +73,7 @@ class SimpleImage {
             imagegif($this->image, $filename);
         } else if($this->image_type == 'IMAGETYPE_PNG') { // png
             imagepng($this->image, $filename);
+        }
         if($permissions != null) {
             chmod($filename, $permissions);
         }
@@ -91,14 +95,14 @@ if(isset($_POST['submit']) && isset($_POST['widthsize'])) {
     $image = new SimpleImage();
     $image->load($_FILES['uploaded_image']['tmp_name']);
     $image->resizeToWidth($_POST['widthsize']);
-    $name = ($_POST['name'])?:time();
-    $image->save("img/".$name.".jpg");
-    header("Location:./img/".$name.".jpg");
+    $name = ($_POST['name'])? $_POST['name'] : time() ;
+    $image->save("./img/".$name.$image->ext);
+    header("Location:./img/".$name.$image->ext);
 } else {
 ?>
 image resize<br>
-<form action=<?=$PHP_SELF;?> method=post enctype="multipart/form-data">
-    image : <input type="file" name="uploaded_image" /><br>
+<form action="simpleimage.php" method=post enctype="multipart/form-data">
+    image (only jpg image) : <input type="file" name="uploaded_image" /><br>
     image resize width(px) : <input type="text" name="widthsize" /><br>
     image name(only english & number) : <input type="text" name="name" /><br>
     <input type="submit" name="submit" value="upload" />
