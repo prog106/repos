d6a7e1594e17713e42684760efa6030efc95b110
prog106 <prog106@gmail.com>
2013-04-10 15:54:58 +0900

    review image

---
+++ review_image.php
@@ -0,0 +1,19 @@
+﻿<!-- view image before image upload -->
+<script type="text/javascript">
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
+
+<body>
+    <form id="form1" runat="server">
+        <input type='file' onchange="readURL(this);" />
+        <img id="blah" src="#" alt="your image" />
+    </form>
+</body>
