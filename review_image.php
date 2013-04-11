99605e96af49c4fd1ce592f5d138d17035e50ad7
prog106 <prog106@gmail.com>
2013-04-11 12:35:45 +0900

    uploadify

--- review_image.php
+++ review_image.php
@@ -18,28 +18,4 @@
         <input type='file' onchange="readURL(this);" />
         <img id="review" src="#" alt="your image" />
     </form>
-    <br><br>
-    ajax file upload : fail<br>
-    <form>
-    <input type="file" id="file" name="file" size="10"/>
-    <input id="uploadbutton" type="button" value="Upload"/>
-    </form>
-    <script>
-        $(document).ready(function () {
-            $("#uploadbutton").click(function () {
-                var filename = $("#file").val();
-                $.ajax({
-                    type: "POST",
-                    url: "review_image.php",
-                    enctype: 'multipart/form-data',
-                    data: {
-                        file: filename
-                    },
-                    success: function (result) {
-                        alert("Data Uploaded: " + result);
-                    }
-                });
-            });
-        });
-    </script>
 </body>
