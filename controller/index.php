eb31a5b37aa5e95714e419b55329c7b3c12ee1ec
prog106 <prog106@gmail.com>
2013-04-26 11:29:05 +0900

    remodelling

--- controller/index.php
+++ controller/index.php
@@ -1,16 +1,16 @@
 <?
 require_once "model/rss.inc";
-class RssController {
+class IndexController {
 
-    function rss_list() {
+    function index_list() {
         $totalcnt = RSS::rsscount();
         $viewpage = ($_GET['page'])? : 1;
-        $countPerPage = 20;
+        $countPerPage = 18;
         $result = RSS::rsslist(($viewpage * $countPerPage) + 1, $countPerPage);
         $page = Util::paging($viewpage, $totalcnt, $countPerPage, '10');
         $param['result'] = $result;
         $param['paging'] = Util::paging_view($page);
-        include "template/rss_list.html";
+        include "template/index.html";
     }
 
 }
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?
 require_once "common/common.inc";
 require_once "common/layout.inc";
-require_once "controller/rss.php";
 head('welcome');
+$view_main = ($_GET['m'] == 'rss')? "rss" : "index";
 
-RssController::rss_list();
-
+switch($view_main) {
+    case "rss":
+        require_once "controller/rss.php";
+        RssController::rss_list();
+    break;
+    default;
+        require_once "controller/index.php";
+        IndexController::index_list();
+    break;
+}
 javascript();
 footer();
 ?>
