1c11fc242f0aac0a1d9e2599236c0e747fa23809
prog106 <prog106@gmail.com>
2013-04-29 19:11:28 +0900

    cron data get

--- controller/index.php
+++ controller/index.php
@@ -3,10 +3,11 @@ require_once "model/rss.inc";
 class IndexController {
 
     function index_list() {
-        $totalcnt = RSS::rsscount();
+        $cron_id = RSS::cronid();
+        $totalcnt = RSS::rsscount($cron_id);
         $viewpage = ($_GET['page'])? : 1;
         $countPerPage = 18;
-        $result = RSS::rsslist(($viewpage * $countPerPage) + 1, $countPerPage);
+        $result = RSS::rsslist(($viewpage * $countPerPage) + 1, $countPerPage, $cron_id);
         $page = Util::paging($viewpage, $totalcnt, $countPerPage, '10');
         $param['result'] = $result;
         $param['paging'] = Util::paging_view($page);
