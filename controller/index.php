3fae5342fe23461b4c1823462807bfd7e6fd406a
prog106 <prog106@gmail.com>
2013-05-10 17:26:53 +0900

    search, gitignore, menu

--- controller/index.php
+++ controller/index.php
@@ -2,12 +2,14 @@
 require_once "model/rss.inc";
 class IndexController {
 
-    function index_list() {
+    function index_list($search=1) {
         $cron_id = RSS::cronid();
         $totalcnt = RSS::rsscount($cron_id);
         $viewpage = ($_GET['page'])? : 1;
-        $countPerPage = 100;
-        $result = RSS::rsslist(($viewpage * $countPerPage) + 1, $countPerPage, $cron_id);
+        $countPerPage = 200;
+        $slimit = ($viewpage == 1)? 0 : ($viewpage * $countPerPage) + 1;
+        $where = ($search == 'area') ? " and d_cate2 = 'Array' " : (($search == 'store') ? " and d_cate2 != 'Array' " : "");
+        $result = RSS::rsslist($slimit, $countPerPage, $cron_id, $where);
         $page = Util::paging($viewpage, $totalcnt, $countPerPage, '10');
         $param['result'] = $result;
         $param['paging'] = Util::paging_view($page);
--- index.php
+++ index.php
@@ -2,18 +2,11 @@
 require_once "common/common.inc";
 require_once "common/layout.inc";
 head('welcome');
-$view_main = ($_GET['m'] == 'rss')? "rss" : "index";
 
-switch($view_main) {
-    case "rss":
-        require_once "controller/rss.php";
-        RssController::rss_list();
-    break;
-    default;
-        require_once "controller/index.php";
-        IndexController::index_list();
-    break;
-}
+$search = ($_GET['list']) ? : 1;
+
+require_once "controller/index.php";
+IndexController::index_list($search);
 javascript();
 footer();
 ?>
