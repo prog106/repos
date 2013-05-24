4b49c817e3164592b07cb7e31f407d89457c0c0e
prog106 <prog106@gmail.com>
2013-05-24 12:25:46 +0900

    rsync exclude.list

--- batch/shell.php
+++ batch/shell.php
@@ -1,7 +1,10 @@
 <?
-//$run = " --dry-run ";
-//$command = "rsync -avrz ".$run." --delete -e -progress /home/prog106/www/batch/ /home/prog106/www/__batch/";
+$run = " --dry-run ";
+$exclude = " --exclude-from=exclude.list";
+$command = "rsync -avrz ".$run." --delete -e -progress /home/prog106/www/batch/ /home/prog106/www/__batch/".$exclude;
+
 $command = "df -h";
+
 $result = shell_exec($command);
 echo "<pre>".$result."</pre>";
 ?>
